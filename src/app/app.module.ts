09a4a97670a093ff10370872eff43234088d84d2
Rybak Maksym <maksym.rybak@sifex-tech.com>
2025-06-23 14:20:02 +0300

    add: semi rule to the prettier

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
-import { Module } from '@nestjs/common'
-import { ConfigModule, ConfigService } from '@nestjs/config'
-import { MongooseModule } from '@nestjs/mongoose'
-import { getDbConfig } from 'src/config/database.config'
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { getDbConfig } from 'src/config/database.config';
 
 @Module({
   imports: [
