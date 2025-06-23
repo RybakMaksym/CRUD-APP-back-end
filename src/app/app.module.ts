efb243fb1ae5381cad0372db5ec2111820d3ab09
Rybak Maksym <maksym.rybak@sifex-tech.com>
2025-06-23 12:53:05 +0300

    add: db connectiong to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,17 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { MongooseModule } from '@nestjs/mongoose'
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('DB_CONNECTION_URL'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
 })
 export class AppModule {}
