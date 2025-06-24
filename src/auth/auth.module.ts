472d15f9346af8be531901c718de9169b28040cc
Rybak Maksym <maksym.rybak@sifex-tech.com>
2025-06-24 12:00:04 +0300

    add: auth module

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { getJWTConfig } from 'config/jwt.config';
+
+import { UserModule } from 'user/user.module';
+
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { JWTStrategy } from './jwt.strategy';
+
+@Module({
+  imports: [
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: getJWTConfig,
+    }),
+    UserModule,
+  ],
+  controllers: [AuthController],
+  providers: [AuthService, JWTStrategy],
+})
+export class AuthModule {}
