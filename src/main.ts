4ead821ff35944054423ad4c73b17b50913ca44c
Rybak Maksym <maksym.rybak@sifex-tech.com>
2025-06-24 11:52:39 +0300

    enable: cors & cookie parser

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import * as cookieParser from 'cookie-parser';
 
 import { AppModule } from 'app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(cookieParser());
+  app.enableCors({
+    origin: process.env.CLIENT_URL ?? 'http://localhost:3001',
+    credentials: true,
+    exposedHeaders: 'set-cookie',
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
