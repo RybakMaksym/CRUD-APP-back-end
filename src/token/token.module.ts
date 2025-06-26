045a0d52bcdcb39f569e1a6c629860d1cc211693
Rybak Maksym <maksym.rybak@sifex-tech.com>
2025-06-26 15:07:38 +0300

    remove: jwt config

--- src/token/token.module.ts
+++ src/token/token.module.ts
@@ -1,21 +1,11 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { JwtModule } from '@nestjs/jwt';
 
-import { getJWTConfig } from 'config/jwt.config';
 import { TokenController } from 'token/token.controller';
 import { TokenService } from 'token/token.service';
 import { UserModule } from 'user/user.module';
 
 @Module({
-  imports: [
-    JwtModule.registerAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: getJWTConfig,
-    }),
-    UserModule,
-  ],
+  imports: [UserModule],
   controllers: [TokenController],
   providers: [TokenService],
   exports: [TokenService],
