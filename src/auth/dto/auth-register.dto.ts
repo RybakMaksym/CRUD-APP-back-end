2742f1d01217db439c6d808ee90d87d520036863
Rybak Maksym <maksym.rybak@sifex-tech.com>
2025-06-27 12:00:38 +0300

    add: password validation

--- src/auth/dto/auth-register.dto.ts
+++ src/auth/dto/auth-register.dto.ts
@@ -1,4 +1,11 @@
-import { IsEmail, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  Length,
+  Matches,
+} from 'class-validator';
 
 import { Role } from 'user/types/role';
 
@@ -14,6 +21,13 @@ export class AuthRegisterDTO {
 
   @IsNotEmpty()
   @IsString()
+  @Length(8, 32, {
+    message: 'Password must be between 8 and 32 characters',
+  })
+  @Matches(/^[^\s'"`\\]+$/, {
+    message:
+      'Password must not contain spaces or invalid characters like quotes or backslashes',
+  })
   password: string;
 
   @IsOptional()
