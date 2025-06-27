8c6a35b9d5d6774a8d28439947259bef2af72f84
Rybak Maksym <maksym.rybak@sifex-tech.com>
2025-06-27 09:53:46 +0300

    add: jwt validate result type

--- src/auth/strategies/access-token-strategy.ts
+++ src/auth/strategies/access-token-strategy.ts
@@ -3,6 +3,8 @@ import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 
+import { JwtReValidateResult } from 'auth/types/jwt-validate-result';
+
 type JwtPayload = {
   id: string;
 };
@@ -19,7 +21,7 @@ export class AccessTokenStrategy extends PassportStrategy(
     });
   }
 
-  async validate(payload: JwtPayload) {
+  public async validate(payload: JwtPayload): JwtReValidateResult {
     return { userId: payload.id };
   }
 }
---
+++ src/auth/types/jwt-validate-result.ts
@@ -0,0 +1,3 @@
+export type JwtReValidateResult = Promise<{
+  userId: string;
+}>;
