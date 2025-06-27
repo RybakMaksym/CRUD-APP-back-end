01eb5e0ed05087aefb9178be176f78ba3692ba2b
Rybak Maksym <maksym.rybak@sifex-tech.com>
2025-06-27 11:22:42 +0300

    delete: REFRESH_TOKEN_NAME

--- env.d.ts
+++ env.d.ts
@@ -5,7 +5,6 @@ declare namespace NodeJS {
     ACCESS_TOKEN_SECRET: string;
     REFRESH_TOKEN_SECRET: string;
     SALT_ROUNDS: number;
-    REFRESH_TOKEN_NAME: string;
     EXPIRE_DAY_ACCESS_TOKEN: string;
     EXPIRE_DAY_REFRESH_TOKEN: string;
   }
