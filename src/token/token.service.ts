ad746c408d2ea508c692ef2c1f931cfc3ecd53c6
Rybak Maksym <maksym.rybak@sifex-tech.com>
2025-06-27 09:41:15 +0300

    replace: proccess env with config service

--- src/token/token.service.ts
+++ src/token/token.service.ts
@@ -1,21 +1,26 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
+
 import { ITokens } from 'token/types/tokens';
 
 @Injectable()
 export class TokenService {
-  constructor(private readonly jwtService: JwtService) {}
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly configService: ConfigService,
+  ) {}
 
   public generateJwtTokens(userId: string): ITokens {
     const payload = { id: userId };
 
     return {
       accessToken: this.jwtService.sign(payload, {
-        secret: process.env.ACCESS_TOKEN_SECRET,
+        secret: this.configService.get<string>('ACCESS_TOKEN_SECRET'),
         expiresIn: `${process.env.EXPIRE_DAY_ACCESS_TOKEN}d`,
       }),
       refreshToken: this.jwtService.sign(payload, {
-        secret: process.env.REFRESH_TOKEN_SECRET,
+        secret: this.configService.get<string>('REFRESH_TOKEN_SECRET'),
         expiresIn: `${process.env.EXPIRE_DAY_REFRESH_TOKEN}d`,
       }),
     };
@@ -25,10 +30,11 @@ export class TokenService {
     const result = await this.jwtService.verifyAsync<{ id: string }>(
       refreshToken,
       {
-        secret: process.env.REFRESH_TOKEN_SECRET,
+        secret: this.configService.get<string>('REFRESH_TOKEN_SECRET'),
       },
     );
     if (!result?.id) throw new UnauthorizedException('Unauthorized');
+
     return result.id;
   }
 }
