582eac9e6829e48ef44269fbe6b57496cbb08ca7
Rybak Maksym <maksym.rybak@sifex-tech.com>
2025-06-30 17:21:30 +0300

    fix: password validation in dtos

--- src/auth/dto/auth-log-in.dto.ts
+++ src/auth/dto/auth-log-in.dto.ts
@@ -1,4 +1,10 @@
-import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  Length,
+  Matches,
+} from 'class-validator';
 
 export class LogInUserDTO {
   @IsNotEmpty()
@@ -8,5 +14,12 @@ export class LogInUserDTO {
 
   @IsNotEmpty()
   @IsString()
+  @Length(8, 32, {
+    message: 'Password must be between 8 and 32 characters',
+  })
+  @Matches(/^[^\s'"`\\]+$/, {
+    message:
+      'Password must not contain spaces or invalid characters like quotes or backslashes',
+  })
   public password: string;
 }
--- src/auth/dto/auth-register.dto.ts
+++ src/auth/dto/auth-register.dto.ts
@@ -1,12 +1,5 @@
 import { PartialType } from '@nestjs/mapped-types';
-import {
-  IsBoolean,
-  IsNotEmpty,
-  IsOptional,
-  IsString,
-  Length,
-  Matches,
-} from 'class-validator';
+import { IsBoolean, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 import { LogInUserDTO } from 'auth/dto/auth-log-in.dto';
 
@@ -14,17 +7,6 @@ export class RegisterUserDTO extends PartialType(LogInUserDTO) {
   @IsNotEmpty()
   @IsString()
   public username: string;
-
-  @IsNotEmpty()
-  @IsString()
-  @Length(8, 32, {
-    message: 'Password must be between 8 and 32 characters',
-  })
-  @Matches(/^[^\s'"`\\]+$/, {
-    message:
-      'Password must not contain spaces or invalid characters like quotes or backslashes',
-  })
-  public password: string;
 
   @IsOptional()
   @IsBoolean()
