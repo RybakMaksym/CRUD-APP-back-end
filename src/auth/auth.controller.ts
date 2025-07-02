2bbe2ca0e8e496f8cc33a647db786819c1231d50
Rybak Maksym <maksym.rybak@sifex-tech.com>
2025-07-02 15:08:42 +0300

    add: log out logic

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,11 @@
 import { Body, Controller, Post } from '@nestjs/common';
 
 import { AuthService } from 'auth/auth.service';
+import { CurrentUserId } from 'auth/decorators/current-user-id.decorator';
 import { CreateUserDTO } from 'auth/dto/create-user.dto';
 import { LogInUserDTO } from 'auth/dto/log-in-user.dto';
 import { IAuthResponse } from 'auth/types/auth.response';
+import { LogOutResponse } from 'auth/types/log-out.response';
 
 @Controller('auth')
 export class AuthController {
@@ -17,5 +19,16 @@ export class AuthController {
   @Post('log-in')
   public async logIn(@Body() dto: LogInUserDTO): Promise<IAuthResponse> {
     return this.authService.logInUser(dto);
+  }
+
+  @Post('log-out')
+  public async logOut(
+    @CurrentUserId() userId: string,
+  ): Promise<LogOutResponse> {
+    await this.authService.logOutUser(userId);
+
+    return {
+      message: 'Log out successfully',
+    };
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -66,4 +66,8 @@ export class AuthService {
       ...tokens,
     };
   }
+
+  public async logOutUser(userId: string): Promise<void> {
+    await this.userService.update(userId, { refreshToken: null });
+  }
 }
