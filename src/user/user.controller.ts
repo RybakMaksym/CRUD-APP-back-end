46c2cb1cb959d684b6d26863dbb70f10958dc2da
Rybak Maksym <maksym.rybak@sifex-tech.com>
2025-07-07 10:01:46 +0300

    add: me & list users endpoints

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,8 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Controller, ForbiddenException, Get, UseGuards } from '@nestjs/common';
 
 import { AccessTokenGuard } from 'auth/guards/access-token.guard';
+import { GetUserId } from 'decorators/get-user-id.decorator';
+import { Role } from 'enums/role.enum';
 import { IUser } from 'user/types/user';
 import { UserService } from 'user/user.service';
 
@@ -10,7 +12,19 @@ export class UserController {
 
   @Get('list')
   @UseGuards(AccessTokenGuard)
-  public async findAllUsers(): Promise<IUser[]> {
+  public async findAllUsers(@GetUserId() userId: string): Promise<IUser[]> {
+    const user = await this.userService.findById(userId);
+
+    if (user.role !== Role.Admin) {
+      throw new ForbiddenException('You do not have access to this resource');
+    }
+
     return this.userService.findAll();
+  }
+
+  @Get('me')
+  @UseGuards(AccessTokenGuard)
+  public async findUserById(@GetUserId() userId: string): Promise<IUser> {
+    return this.userService.findById(userId);
   }
 }
