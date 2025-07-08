1de3055ec9208ff60830f64c8270c8fbd0d7c0d7
RybakMaksym <maksym.rybak@sifex-tech.com>
2025-07-08 10:37:00 +0300

    add: log out logic
    * add: log out logic
    
    * add: auth guard to log out endpoint
    
    * fix: log out logic
    
    * delete: get user decorator
    
    * rename: ISimpleMessage to IMessageResponse
    
    * rename: message interfaces file

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,19 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Headers, Post, UseGuards } from '@nestjs/common';
 
 import { AuthService } from 'auth/auth.service';
 import { CreateUserDTO } from 'auth/dto/create-user.dto';
 import { LogInUserDTO } from 'auth/dto/log-in-user.dto';
+import { RefreshTokenGuard } from 'auth/guards/refresh-token.guard';
 import { IAuthResponse } from 'auth/types/auth.response';
+import { TokenService } from 'token/token.service';
+import { IMessageReponse } from 'types/message.interfaces';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly tokenService: TokenService,
+  ) {}
 
   @Post('register')
   public async register(@Body() dto: CreateUserDTO): Promise<IAuthResponse> {
@@ -17,5 +23,20 @@ export class AuthController {
   @Post('log-in')
   public async logIn(@Body() dto: LogInUserDTO): Promise<IAuthResponse> {
     return this.authService.logInUser(dto);
+  }
+
+  @Post('log-out')
+  @UseGuards(RefreshTokenGuard)
+  public async logOut(
+    @Headers('authorization') authorization?: string,
+  ): Promise<IMessageReponse> {
+    const refreshToken = authorization?.split(' ')[1];
+
+    const userId = await this.tokenService.verifyToken(refreshToken);
+    await this.authService.logOutUser(userId);
+
+    return {
+      message: 'Log out successfully',
+    };
   }
 }
