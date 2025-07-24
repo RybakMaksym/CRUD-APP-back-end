0ce5b275504fd11eb98406f6ac7afb284d0939d2
Rybak Maksym <maksym.rybak@sifex-tech.com>
2025-07-24 09:18:18 +0300

    add: patterns constants

--- src/helpers/escape-regex.ts
+++ src/helpers/escape-regex.ts
@@ -1,3 +1,5 @@
+import { REGEX_SPECIAL_CHARS } from '@/constants/patterns.constants';
+
 export const escapeRegex = (value: string): string => {
-  return value.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+  return value.replace(REGEX_SPECIAL_CHARS, '\\$&');
 };
